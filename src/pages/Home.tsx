346a040fed13170a4ed5e58865fdc4e46d3f7e37
Alvie <alvaro.santana23@gmail.com>
2026-03-09 23:46:37 -0600

    Remove link in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 import UpcomingEvents from "../components/UpcomingEvents";
 import { FaDiscord } from "react-icons/fa";
